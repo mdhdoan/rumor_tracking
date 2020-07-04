11b4c5219caf9df76143403cbf8d803d74b1685c
minh <mdhdoan@gmail.com>
2020-07-04 14:51:14 -0400

    added csv_file

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import networkx
+# Author: Minh
+import csv, sys
 
-nx = networkx
-file = 'higgs-retweet_network.edgelist'
-filename = open(file, 'rb')
-print(type(filename))
-graph_data = nx.read_edgelist(filename, nodetype = int, create_using=nx.DiGraph) 
-print(graph_data.nodes())
-filename.close()
+def csv_delimitize(filename, new_content):
+    file = open(filename, 'r')
+    for lines in file:
+        new_lines = lines.replace(' ', ',')
+        new_content.append(new_lines)
+    file.close()
+
+def write_csv(f_name):
+    new_content = []
+    csv_delimitize(f_name, new_content)
+    new_file = open('twitter_activity.csv', 'w')
+    for line in new_content:
+        new_file.write(line)
+    new_file.close()
+if __name__ == "__main__":
+    f_name = sys.argv[1]
+    write_csv(f_name)
+    
